1d9cad8107e4b83fceef801fa317c220ef0d5507
ChrisTangzh <1045493988@qq.com>
2020-06-27 01:53:23 +0800

    register.html v1.0

--- TaskSaas/views.py
+++ TaskSaas/views.py
@@ -36,11 +36,17 @@ class RegisterModelForm(forms.ModelForm):
                                    validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号码格式错误'), ])
     password = forms.CharField(label='密码', widget=forms.PasswordInput())
     confirm_password = forms.CharField(label='重复密码', widget=forms.PasswordInput())
-    code = forms.CharField(label='验证码')
+    code = forms.CharField(label='验证码', widget=forms.TextInput())
 
     class Meta:
         model = models.UserInfo
-        fields = "__all__"
+        fields = ['username','email','password','confirm_password','mobile_phone','code']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = '请输入%s' % (field.label,)
 
 
 def register(request):
