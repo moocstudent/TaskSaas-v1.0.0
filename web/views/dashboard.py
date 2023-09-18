5e8b3f0ea56713703eb1501d5d9344db38660ae7
moocstudent <deadzq@qq.com>
2023-09-18 12:05:36 +0800

    echarts demo

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -9,6 +9,8 @@ def dashboard(request, project_id):
     # 项目状态
     status_dict = collections.OrderedDict()
 
+    echarts_data = []
+
     for key, text in sorted(models.Issues.status_choices):
         status_dict[key] = {'text': text, 'count': 0}
 
@@ -18,15 +20,25 @@ def dashboard(request, project_id):
     for item in issues_data:
         status_dict[item['status']]['count'] = item['ct']
 
+    for item in sorted(status_dict.items()):
+        # print('key',d)
+        print('item',)
+        print('item count',item[1]['count'])
+        echarts_data.insert(item[0]-1,item[1]['count'])
+        # print('count',d['count'])
+        # echarts_data[d['count']]
+
     join_user = models.ProjectUser.objects.filter(project_id=project_id).values_list('user_id', 'user__username')
 
     print('join_user',join_user)
+    print('echarts_data',echarts_data)
     top_ten = models.Issues.objects.filter(project_id=project_id, assign__isnull=False).order_by('-id')[0:10]
 
     context = {
         'status_dict': status_dict,
         'join_user': join_user,
-        'top_ten': top_ten
+        'top_ten': top_ten,
+        'echarts_data': echarts_data
     }
 
     return render(request, 'web/dashboard.html', context)
